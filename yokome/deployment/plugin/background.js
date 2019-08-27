46a0e1cfcedba7f0cfb8d6f0fecf1afbf631fa9c
Julian Betz <33902330+julianbetz@users.noreply.github.com>
2019-08-27 14:15:03 +0200

    Add licensing information

--- yokome/deployment/plugin/background.js
+++ yokome/deployment/plugin/background.js
@@ -1,22 +1,27 @@
+// -*- coding: utf-8 -*-
+
+/**
+ * Copyright 2019 Julian Betz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 const TITLE_ACTIVATE = "Activate Yokome"
 const TITLE_DEACTIVATE = "Deactivate Yokome"
 const TITLE_INACTIVE = "Yokome: Only Works on HTTP Pages"
 const APPLICABLE_PROTOCOLS = ["http:", "https:"]
 
-// function toggleYokome() {
-//     function on(tabs) {
-//         browser.tabs.insertCSS({code: 'body {background-color: #3287dc;}'}).then(function () {
-//             browser.tabs.sendMessage(tabs[0].id, {
-//                 command: 'on'
-//             });
-//         });
-//     }
-//     browser.console.log('Yokome on');
-// }
-
-// browser.pageAction.onClicked.addListener(function (tab) {
-//     console.log(true);
-// });
 
 function toggleYokome(tab) {
     function toggle(action) {
@@ -35,6 +40,7 @@ function toggleYokome(tab) {
     getAction.then(toggle);
 }
 
+
 function initializePageAction(tab) {
     if (APPLICABLE_PROTOCOLS.includes(new URL(tab.url).protocol)) {
         // browser.pageAction.setIcon({tabId: tab.id, path: ""});
@@ -45,6 +51,7 @@ function initializePageAction(tab) {
     }
 }
 
+
 // Initialize Yokome for all tabs on first load
 var getAllTabs = browser.tabs.query({});
 getAllTabs.then((tabs) => {
@@ -52,6 +59,7 @@ getAllTabs.then((tabs) => {
         initializePageAction(tab);
     }
 });
+
 // Reset Yokome for a tab on that tab's update
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
     initializePageAction(tab);
